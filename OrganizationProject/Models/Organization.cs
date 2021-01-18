698d41dc085d2da257a83bfcdaa90b2d6d1e35d7
lglayat <luke.glayat60@gmail.com>
2021-01-17 19:48:07 -0500

    added my solution

---
+++ OrganizationProject/Models/Organization.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrganizationProject.Controllers
+{
+    public class Organization
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int BlackListTotal { get; set; }
+        public int TotalCount { get; set; }
+
+        public List<User> Users { get; set; }
+    }
+}
---
+++ OrganizationProject/Models/Phone.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrganizationProject.Controllers
+{
+    public class Phone
+    {
+        public string Id { get; set; }
+        public string UserId { get; set; }
+        public Boolean Blacklist { get; set; }
+    }
+}
